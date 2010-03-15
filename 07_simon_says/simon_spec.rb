c5efc3c9cc3e034abbc1b53afbbe0f441a399799
Sarah Allen <sarah@ultrasaurus.com>
2010-03-15 15:54:20 -0700

    fixed simon spec error

--- 07_simon_says/simon_spec.rb
+++ 07_simon_says/simon_spec.rb
@@ -1,42 +1,44 @@
-describe "Simon says" do
+require 'simon'
+
+describe Simon do
   
   it "should echo hello" do
-    echo("hello").should == "hello"
+    Simon.new.echo("hello").should == "hello"
   end
   
   it "should echo bye" do
-    echo("bye").should == "bye"
+    Simon.new.echo("bye").should == "bye"
   end
 
   it "should shout hello" do
-    shout("hello").should == "HELLO"
+    Simon.new.shout("hello").should == "HELLO"
   end
   
   it "should shout multiple words" do
-    shout("hello world").should == "HELLO WORLD"
+    Simon.new.shout("hello world").should == "HELLO WORLD"
   end
 
   it "should repeat" do
-    repeat("hello").should == "hello hello"
+    Simon.new.repeat("hello").should == "hello hello"
   end
 
   it "should repeat a number of times" do
-    repeat("hello", 3).should == "hello hello hello"
+    Simon.new.repeat("hello", 3).should == "hello hello hello"
   end
 
   it "should return the first letter" do
-    start_of_word("hello", 1).should == "h"
+    Simon.new.start_of_word("hello", 1).should == "h"
   end
   
   it "should return the first two letters" do
-    start_of_word("Bob", 2).should == "Bo"
+    Simon.new.start_of_word("Bob", 2).should == "Bo"
   end
 
   it "should tell us the first word of 'Hello World' is 'Hello'" do
-    first_word("Hello World").should == "Hello"
+    Simon.new.first_word("Hello World").should == "Hello"
   end
 
   it "should tell us the first word of 'oh dear' is 'oh'" do
-    first_word("Hello World").should == "oh"
+    Simon.new.first_word("oh dear").should == "oh"
   end
 end
