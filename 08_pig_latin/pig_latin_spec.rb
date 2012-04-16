24b9b51c20b999a115fcb35618240fe98c0615d6
Curtis Schofield <curtis@ram9.cc>
2012-04-16 14:21:53 -0700

    added edge case

--- 08_pig_latin/pig_latin_spec.rb
+++ 08_pig_latin/pig_latin_spec.rb
@@ -9,27 +9,28 @@ describe "#translate" do
   end
 
   it "should translate a word beginning with a vowel" do
-    pending
     s = translate("apple")
     s.should == "appleay"
   end
 
   it "should translate a two consonent word: stupid" do
-    pending
     s = translate("stupid")
     s.should == "upidstay"
   end
 
   it "should translate two words: eat pie" do
-    pending
     s = translate("eat pie")
     s.should == "eatay iepay"
   end
 
   it "should translate multiple words: the quick brown fox" do
-    pending
     s = translate("the quick brown fox")
     s.should == "ethay ickquay ownbray oxfay"
+  end
+
+  it "should not translate 'tu' as 'qu' " do
+    s = translate("the quick turn")
+    s.should == "ethay ickquay urntay"
   end
 
 end
